b13ace2af10707ad6156dc96fd77b77161ca6729
Aaron Benmark <benmarkaa@gmail.com>
2012-12-13 18:46:58 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
 
   before {  @user = User.new(name: "Example User", email: "user@example.com",
   							password: "foobar", password_confirmation: "foobar")  }
+  
   subject {  @user  }
 
   it {  should respond_to(:name)  }
@@ -22,6 +23,7 @@ describe User do
   it {  should respond_to(:password_digest)  }
   it {  should respond_to(:password)  }
   it {  should respond_to(:password_confirmation)  }
+  it {  should respond_to(:remember_token)  }
   it {  should respond_to(:authenticate)  }
 
   it {  should be_valid  }
@@ -107,5 +109,9 @@ describe User do
     before {  @user.password = @user.password_confirmation = "a" * 5  }
     it {  should be_invalid  }
   end
-   
+
+  describe "remember token" do
+    before {  @user.save  }
+    its(:remember_token) {  should_not be_blank  }
+  end  
 end
